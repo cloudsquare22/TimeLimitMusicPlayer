b148394c6702064889e6037044bdf4ae2a28865b
shi-n <shi-n_xyz@cloudsquare.jp>
2020-11-26 06:47:27 +0900

    Initial Commit

---
+++ TimeLimitMusicPlayer/TimeLimitMusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeLimitMusicPlayerApp.swift
+//  TimeLimitMusicPlayer
+//
+//  Created by Shin Inaba on 2020/11/26.
+//
+
+import SwiftUI
+
+@main
+struct TimeLimitMusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
